51cc8f2fbec80b87a126a8dde20e0e7242cae254
ASUS <ASUS@DESKTOP-HVR68K9>
2018-12-16 22:02:31 +0700

    Chat Aplikasi(ChatAjaKapanJadian)

---
+++ src/chatajakapanjadian/ChatServer.java
@@ -0,0 +1,111 @@
+
+package chatajakapanjadian;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+/**
+ *
+ * @author ASUS
+ */
+public class ChatServer {
+     private static final int PORT = 1500;
+    private static HashSet<String> names = new HashSet<String>();
+   
+    private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("The chat server is running.");
+        ServerSocket listener = new ServerSocket(PORT);
+        try {
+            while (true) {
+                new Handler(listener.accept()).start();
+            }
+        } finally {
+            listener.close();
+        }
+    }
+
+ 
+
+    private static class Handler extends Thread {
+        private String name;
+        private Socket socket;
+        private BufferedReader in;
+        private PrintWriter out;
+        public String printTimeStamp;
+
+        //Socket
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+       
+        public void run() {
+            try {
+                // Membuat Alamat Socket
+                in = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+              
+                while (true) {
+                    out.println("SUBMITNAME");
+                    name = in.readLine();
+                    if (name == null) {
+                        return;
+                    }
+                    synchronized (names) {
+                        if (!names.contains(name)) {
+                            names.add(name);
+                            break;
+                        }
+                    }
+                }
+
+               
+                out.println("NAMEACCEPTED");
+                writers.add(out);
+                
+                // Menerima pesan dari client ini untuk kemudian membroadcastnya.
+                while (true) {
+                    String input = in.readLine();
+                    if (input == null) {
+                        return;
+                    }
+                    for (PrintWriter writer : writers) {
+                        DateFormat df = new SimpleDateFormat ("HH:mm");
+                        Date dateobj = new Date();
+                        writer.println("MESSAGE " + name + ": " + input + " " + "(" + df.format(dateobj) + ")");
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            } finally {
+               
+                if (name != null) {
+                    names.remove(name);
+                }
+                if (out != null) {
+                    writers.remove(out);
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+        
+    }
+}
+ 
